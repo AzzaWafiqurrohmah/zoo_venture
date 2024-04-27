518c4497fe949b5adeff764f920672f653e1dcc9
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2024-04-27 18:13:46 +0700

    fix: api endpoint

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(ShedController::class)->group(function(){
-    Route::get('/all', 'json');
-    Route::post('/byId', 'apiById');
-});
+Route::prefix('sheds')
+    ->controller(ShedController::class)->group(function () {
+        Route::get('/', 'json');
+        Route::get('/{shed}', 'apiById');
+    });
